39a0dba344d35f247143af76a0ff9d74a9bbc4b7
vkryl <6242627+vkryl@users.noreply.github.com>
2022-04-17 20:55:59 +0400

    Migrate to ReferenceList<View> in MultipleViewProvider

--- src/main/java/me/vkryl/android/util/MultipleViewProvider.java
+++ src/main/java/me/vkryl/android/util/MultipleViewProvider.java
@@ -26,15 +26,11 @@ import android.os.Looper;
 import android.view.View;
 import android.view.ViewParent;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.lang.ref.Reference;
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
-
 import me.vkryl.android.ViewUtils;
-import me.vkryl.core.reference.ReferenceUtils;
+import me.vkryl.core.reference.ReferenceList;
 
 public class MultipleViewProvider implements ViewProvider {
   public interface InvalidateContentProvider {
@@ -66,196 +62,82 @@ public class MultipleViewProvider implements ViewProvider {
     return this;
   }
 
-  private @Nullable List<Reference<View>> views;
+  private final ReferenceList<View> views = new ReferenceList<>(false, false, null);
 
-  public @Nullable List<Reference<View>> getViewsList () {
+  public @NonNull ReferenceList<View> getViewsList () {
     return views;
   }
 
   public final boolean attachToView (@Nullable View view) {
-    if (view != null) {
-      if (views == null) {
-        views = new ArrayList<>(2);
-      }
-
-      if (views.isEmpty()) {
-        views.add(new WeakReference<>(view));
-        return true;
-      }
-
-      boolean found = false;
-      final int size = views.size();
-      for (int i = size - 1; i >= 0; i--) {
-        View currentView = views.get(i).get();
-        if (currentView == null) {
-          views.remove(i);
-        } else if (currentView.equals(view)) {
-          found = true;
-        }
-      }
-
-      if (!found) {
-        views.add(new WeakReference<>(view));
-        return true;
-      }
-    } else {
-      ReferenceUtils.gcReferenceList(views);
-    }
-
-    return false;
+    return view != null && views.add(view);
   }
 
   public final boolean detachFromView (@Nullable View view) {
-    if (views != null) {
-      if (view != null) {
-        boolean found = false;
-        final int size = views.size();
-        for (int i = size - 1; i >= 0; i--) {
-          View currentView = views.get(i).get();
-          if (currentView == null) {
-            views.remove(i);
-          } else if (currentView.equals(view)) {
-            views.remove(i);
-            found = true;
-          }
-        }
-        return found;
-      } else {
-        ReferenceUtils.gcReferenceList(views);
-      }
-    }
-    return false;
+    return view != null && views.remove(view);
   }
 
-  public final int detachFromAllViews () {
-    if (views != null) {
-      final int size = views.size();
-      int removedCount = 0;
-      for (int i = size - 1; i >= 0; i--) {
-        View currentView = views.get(i).get();
-        views.remove(i);
-        if (currentView != null) {
-          removedCount++;
-        }
-      }
-      return removedCount;
-    }
-    return 0;
+  public final void detachFromAllViews () {
+    views.clear();
   }
 
   @Override
   public void invalidateParent (int left, int top, int right, int bottom) {
-    if (views != null) {
-      final int size = views.size();
-      for (int i = size - 1; i >= 0; i--) {
-        View view = views.get(i).get();
-        if (view != null) {
-          ViewParent parent = view.getParent();
-          if (parent != null) {
-            ((View) parent).invalidate(left, top, right, bottom);
-          }
-        } else {
-          views.remove(i);
-        }
+    for (View view : views) {
+      ViewParent parent = view.getParent();
+      if (parent != null) {
+        ((View) parent).invalidate(left, top, right, bottom);
       }
     }
   }
 
   @Override
   public void invalidateParent () {
-    if (views != null) {
-      final int size = views.size();
-      for (int i = size - 1; i >= 0; i--) {
-        View view = views.get(i).get();
-        if (view != null) {
-          ViewParent parent = view.getParent();
-          if (parent != null) {
-            ((View) parent).invalidate();
-          }
-        } else {
-          views.remove(i);
-        }
+    for (View view : views) {
+      ViewParent parent = view.getParent();
+      if (parent != null) {
+        ((View) parent).invalidate();
       }
     }
   }
 
   @Override
   public void invalidate () {
-    if (views != null) {
-      final int size = views.size();
-      for (int i = size - 1; i >= 0; i--) {
-        View view = views.get(i).get();
-        if (view != null) {
-          view.invalidate();
-        } else {
-          views.remove(i);
-        }
-      }
+    for (View view : views) {
+      view.invalidate();
     }
   }
 
   @Override
   public void invalidateOutline (boolean withInvalidate) {
-    if (views != null) {
-      final int size = views.size();
-      for (int i = size - 1; i >= 0; i--) {
-        View view = views.get(i).get();
-        if (view != null) {
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            view.invalidateOutline();
-          }
-          if (withInvalidate) {
-            view.invalidate();
-          }
-        } else {
-          views.remove(i);
-        }
+    for (View view : views) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        view.invalidateOutline();
+      }
+      if (withInvalidate) {
+        view.invalidate();
       }
     }
   }
 
   @Override
   public void invalidate (int left, int top, int right, int bottom) {
-    if (views != null) {
-      final int size = views.size();
-      for (int i = size - 1; i >= 0; i--) {
-        View view = views.get(i).get();
-        if (view != null) {
-          view.invalidate(left, top, right, bottom);
-        } else {
-          views.remove(i);
-        }
-      }
+    for (View view : views) {
+      view.invalidate(left, top, right, bottom);
     }
   }
 
   @Override
   public void invalidate (Rect dirty) {
-    if (views != null) {
-      final int size = views.size();
-      for (int i = size - 1; i >= 0; i--) {
-        View view = views.get(i).get();
-        if (view != null) {
-          view.invalidate(dirty);
-        } else {
-          views.remove(i);
-        }
-      }
+    for (View view : views) {
+      view.invalidate(dirty);
     }
   }
 
   @Override
   public boolean belongsToProvider (View childView) {
-    if (views != null) {
-      final int size = views.size();
-      for (int i = size - 1; i >= 0; i--) {
-        View view = views.get(i).get();
-        if (view != null) {
-          if (view != childView)
-            continue;
-          return true;
-        }
-        views.remove(i);
+    for (View view : views) {
+      if (view == childView) {
+        return true;
       }
     }
     return false;
@@ -263,22 +145,15 @@ public class MultipleViewProvider implements ViewProvider {
 
   @Override
   public @Nullable View findAnyTarget () {
-    if (views != null) {
-      final int size = views.size();
-      for (int i = size - 1; i >= 0; i--) {
-        View view = views.get(i).get();
-        if (view != null) {
-          return view;
-        }
-        views.remove(i);
-      }
+    for (View view : views) {
+      return view;
     }
     return null;
   }
 
   @Override
   public boolean hasAnyTargetToInvalidate () {
-    return views != null && !views.isEmpty();
+    return !views.isEmpty();
   }
 
   @Override
@@ -296,20 +171,13 @@ public class MultipleViewProvider implements ViewProvider {
 
   @Override
   public void requestLayout () {
-    if (views != null) {
-      boolean isBackground = Looper.myLooper() != Looper.getMainLooper();
-      final int size = views.size();
-      for (int i = size - 1; i >= 0; i--) {
-        View view = views.get(i).get();
-        if (view != null) {
-          if (isBackground) {
-            handler.sendMessage(handler.obtainMessage(ACTION_REQUEST_LAYOUT, view));
-          } else {
-            view.requestLayout();
-          }
-        } else {
-          views.remove(i);
-        }
+    // FIXME: better thread safety
+    final boolean isBackground = Looper.myLooper() != Looper.getMainLooper();
+    for (View view : views) {
+      if (isBackground) {
+        handler.sendMessage(handler.obtainMessage(ACTION_REQUEST_LAYOUT, view));
+      } else {
+        view.requestLayout();
       }
     }
   }
