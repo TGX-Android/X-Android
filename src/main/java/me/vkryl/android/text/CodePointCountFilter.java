593e204292aafb1992193fdde5242de4ec1ffaf1
vkryl <6242627+vkryl@users.noreply.github.com>
2023-03-09 10:59:39 +0400

    Removed current year from copyright

--- src/main/java/me/vkryl/android/text/CodePointCountFilter.java
+++ src/main/java/me/vkryl/android/text/CodePointCountFilter.java
@@ -1,6 +1,6 @@
 /*
  * This file is a part of X-Android
- * Copyright © Vyacheslav Krylov (slavone@protonmail.ch) 2014-2023
+ * Copyright © Vyacheslav Krylov (slavone@protonmail.ch) 2014
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
